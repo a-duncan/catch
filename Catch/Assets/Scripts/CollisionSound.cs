4230ffbe77e5555a4b14b7fd604f956f3ea93119
a-duncan <agduncan@gmail.com>
2020-12-04 10:19:22 -0800

    Added ball hit sound
    Balls now make a sound on collision with other objects. Its volume varies with the relative velocity of the collision and the distance to the listener (camera).
    
    I tried implementing sounds for ice and rock collisions but ran into trouble. I believe the problem has to do with playing too many sounds at once.

---
+++ Catch/Assets/Scripts/CollisionSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioSource collisionSound;
+
+    public float relativeVelocityThreshold = 1f;
+    public float fullVolumeVelocity = 20f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float velocity = collision.relativeVelocity.magnitude;
+
+        if (velocity > relativeVelocityThreshold)
+        {
+            float vol = Mathf.Min(1f, velocity / fullVolumeVelocity);
+            collisionSound.PlayOneShot(collisionSound.clip, vol);
+        }
+    }
+}
