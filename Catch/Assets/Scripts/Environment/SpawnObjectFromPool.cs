25ff12ac97a0d2f71e8db6b67f8fdd18f6b77d74
a-duncan <agduncan@gmail.com>
2020-12-07 12:42:21 -0800

    Hail spawn randomness
    - Increased height of spawning region to get rid of wave effect.
    - Added random velocity and angular velocity to newly-spawned hailstones.

--- Catch/Assets/Scripts/Environment/SpawnObjectFromPool.cs
+++ Catch/Assets/Scripts/Environment/SpawnObjectFromPool.cs
@@ -12,6 +12,9 @@ public class SpawnObjectFromPool : MonoBehaviour
 
     public Vector3 spawnPosJitter;
 
+    public Vector3 spawnVelocity;
+    public Vector3 spawnVelJitter;
+    public float maxSpin = 0;
 
     float timer = 1f;
 
@@ -34,7 +37,18 @@ public class SpawnObjectFromPool : MonoBehaviour
             spawnPosition += Vector3.forward * spawnPosJitter.z * (Random.value - 0.5f);
             spawnPosition += Vector3.up * spawnPosJitter.y * (Random.value - 0.5f);
 
-            EasyObjectPool.instance.GetObjectFromPool(poolName, spawnPosition, Quaternion.identity);
+            GameObject obj = EasyObjectPool.instance.GetObjectFromPool(poolName, spawnPosition, Quaternion.identity);
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.velocity = spawnVelocity;
+                rb.velocity += Vector3.right * spawnVelJitter.x * (Random.value - 0.5f);
+                rb.velocity += Vector3.forward * spawnVelJitter.z * (Random.value - 0.5f);
+                rb.velocity += Vector3.up * spawnVelJitter.y * (Random.value - 0.5f);
+
+                rb.angularVelocity = maxSpin * Random.insideUnitSphere;
+            }
         }
     }
 }
