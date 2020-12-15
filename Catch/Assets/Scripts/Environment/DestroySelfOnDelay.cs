9699fa0f209ee9f67a389ee2da210a61c71c046d
a-duncan <agduncan@gmail.com>
2020-12-14 16:38:38 -0800

    Visual FX for basket in pong
    When you make a basket in SpacePong, there is now a particle burst effect that appears when the basket disappears.

---
+++ Catch/Assets/Scripts/Environment/DestroySelfOnDelay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelfOnDelay : MonoBehaviour
+{
+    public float delayTime = 1f;
+
+    float timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = delayTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0f)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Catch/Assets/Scripts/Environment/PongBasket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PongBasket : MonoBehaviour
+{
+    public PongLevelManager levelManager;
+    public GameObject basketFX;
+    public GameObject basketGO;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            levelManager.decrBasketsRemaining();
+            levelManager.ResetBall();
+
+            //Quaternion FXRot = Quaternion.Euler(90f, 0f, 0f) * transform.rotation;
+            Instantiate(basketFX, transform.position, transform.rotation);
+            
+            
+            basketGO.SetActive(false);
+        }
+    }
+}
